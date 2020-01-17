3fc1f5ac9800ee2ca4f8c752317aee8228dc0c1c
Ronan O Cuinn <ronan.ocuinn@ig.com>
2020-01-17 16:11:11 +0000

    Continuing chapter 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React app!</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Rónán', age: 28 },
+      { name: 'Bleeks', age: 28 }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React app!</h1>
+        <p>This works.</p>
+        <button>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies: Pints</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Pints</Person>
+      </div>
+    );
+  }
 }
 
 export default App;
